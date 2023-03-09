371a951ebf120e9772d533769286dc2c22a8d56c
ujn-art <62077959+ujn-art@users.noreply.github.com>
2023-03-09 23:52:51 +0800

    Update tictactoe.js
    make the code more clean

--- tictactoe.js
+++ tictactoe.js
@@ -1,83 +1,105 @@
-const a1 = document.querySelector('#a1')
-const a2 = document.querySelector('#a2')
-const a3 = document.querySelector('#a3')
-const b1 = document.querySelector('#b1')
-const b2 = document.querySelector('#b2')
-const b3 = document.querySelector('#b3')
-const c1 = document.querySelector('#c1')
-const c2 = document.querySelector('#c2')
-const c3 = document.querySelector('#c3')
-const pattern1 = [a1, a2, a3]
-const pattern2 = [b1, b2, b3]
-const pattern3 = [c1, c2, c3]
-const pattern4 = [a1, b1, c1]
-const pattern5 = [a2, b2, c2]
-const pattern6 = [a3, b3, c3]
-const pattern7 = [a1, b2, c3]
-const pattern8 = [c1, b2, a3]
-const playerOne = [1, 3, 5, 7, 9]
-const turnModal = document.getElementById('turnModal')
-const resultModal = document.getElementById('resultModal')
-const arr = [pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8]
-const draw = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
-const turnMs = document.getElementById('turnMs')
-const resultMs = document.getElementById('resultMs')
-let turn = 1 // count of turns per player
-// add img to the div element
-function addfunc(value) {
-    const row = document.getElementById(value)
-    if (row.hasChildNodes()) {
-        console.log('yes')
-    } else {
-        const img = document.createElement('img')
-        if (playerOne.includes(turn)) {
-            
-            img.src = 'circle.png'
-            row.appendChild(img)
-            turnMs.innerText = 'Player Two Turn!'
-            turnModal.classList.remove('close')
-            turn++
-        } else {
-            img.src = 'x.png'
-            row.appendChild(img)
-            turnMs.innerText = 'Player One Turn!'
-            turnModal.classList.remove('close')
-            turn++
-        }
+const playerOneScore = document.querySelector("#playerOneScore")
+const playerTwoScore = document.querySelector("#playerTwoScore")
+const board = document.querySelector('#board')
+// winning pattern 
+const pattern = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+]
+if(!localStorage.getItem('playerOne') && !localStorage.getItem('playerOne')){
+    localStorage.setItem('playerOne', 0)
+    localStorage.setItem('playerTwo', 0)
+}
+playerOneScore.innerHTML = localStorage.getItem('playerOne')
+playerTwoScore.innerHTML = localStorage.getItem('playerTwo')
+let playerTurn = true
+// creating cells using javascript 
+function createCells(){
+    const playerOne = document.querySelector('#playerOne').style.color = 'green'
+    const playerTwo = document.querySelector('#playerTwo').style.color = 'red'
+    for(let i = 0; i < 9; i++){ // loop 9 times
+        const square = document.createElement('div') 
+        square.classList.add('square') // add square class in div
+        square.id = i // adding id in div
+        board.append(square) // append the square in board div
     }
-    // check every pattern 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].every((row) => { return row.hasChildNodes() })) { // check if every div has the child nodes
-            if (arr[i].every((row) => { return row.firstElementChild.getAttribute('src') == 'circle.png' })) {
-                console.log('player one wins')
-                resultMs.innerText = 'Player One Wins!'
-                resultModal.classList.remove('close')
-                turnModal.classList.add('close')
-                break
-            } else if (arr[i].every((row) => { return row.firstElementChild.getAttribute('src') == 'x.png' })) {
-                console.log('player two wins')
-                resultMs.innerText = 'Player Two Wins!'
-                resultModal.classList.remove('close')
-                turnModal.classList.add('close')
-                break
-            }else{
-                if (draw.every((row) => { return row.hasChildNodes() })) {
-                    resultMs.innerText = 'Draw!'
-                    resultModal.classList.remove('close')
-                    turnModal.classList.add('close')
-                }
+    const cells = document.querySelectorAll('.square')
+    cells.forEach((cell) => {
+        cell.addEventListener('click', turn)
+    })
+}
+// call the createCells
+createCells()
+function turn(e){
+    const img = document.createElement('img')
+    const playerOne = document.querySelector('#playerOne')
+    const playerTwo = document.querySelector('#playerTwo')
+    if(playerTurn){ // if playerTurn is true player one turn
+        img.src = 'circle.png'
+        e.target.append(img)
+        playerTurn = false // making the playerturn false to make player two turn
+        e.target.removeEventListener('click', turn)
+        playerOne.style.color = 'red'
+        playerTwo.style.color = 'green'
+    }else{
+        img.src = 'x.png'
+        e.target.append(img)
+        playerTurn = true
+        e.target.removeEventListener('click', turn)
+        playerOne.style.color = 'green'
+        playerTwo.style.color = 'red'
+    }
+    checkWinner()
+}
+const msgBox = document.querySelector('.message-box') // message box
+
+// Checking the pattern 
+function checkWinner(){
+    const square = document.querySelectorAll('.square') // get all the div with square class
+    const squareArray = Array.from(square) // convert the  square object in to array
+    const msg = document.querySelector('.message')
+    // loop the pattern
+    pattern.forEach((cells) => { 
+        if( cells.every(cell => square[cell].firstChild?.getAttribute('src') === 'circle.png')){ // check every square if has a childNode and has an attribure of circle.png
+            let playerOne = parseInt(localStorage.getItem('playerOne') )+ 1 // add a score in to localstorage
+            localStorage.setItem("playerOne", playerOne) // set the new score in local storage
+            playerOneScore.innerHTML = localStorage.getItem('playerOne') // get the score and display it using innerHTML
+            playerTwoScore.innerHTML = localStorage.getItem('playerTwo')
+            square.forEach(cell => cell.removeEventListener('click', turn)) // if some win the match it will remove all eventlistener
+            msg.innerHTML = "Player One Win!"
+            msgBox.style.display = 'block' // change the display to block 
+        }
+        else if(cells.every(cell => square[cell].firstChild?.getAttribute('src') === 'x.png')){
+            let playerTwo = parseInt(localStorage.getItem('playerTwo') )+ 1
+            localStorage.setItem("playerTwo", playerTwo)
+            playerOneScore.innerHTML = localStorage.getItem('playerOne')
+            playerTwoScore.innerHTML = localStorage.getItem('playerTwo')
+            square.forEach(cell => cell.removeEventListener('click', turn))
+            msg.innerHTML = "Player Two Win!"
+            msgBox.style.display = 'block'
+        }else{
+            if(squareArray.every(cell => cell.hasChildNodes())){ // check if all the square already have childnodes
+                msg.innerHTML = 'Draw!'
             }
         }
-    }
-
+    })   
+   
 }
-// remove the every img element in document
-function closeTurn() {
-    turnModal.classList.add('close')
+// New Round 
+function newRound(){
+    msgBox.style.display = 'none'
+    board.innerHTML = ''
+    createCells()
+    playerTurn = true
 }
-function reset() {
-    const img = document.querySelectorAll('img');
-    img.forEach(element => element.remove())
-    turn = 1
-    resultModal.classList.add('close')
+// Reset the score
+function reset(){
+    msgBox.style.display = 'none' 
+    board.innerHTML = ''
+    createCells()
+    localStorage.setItem('playerOne', 0)
+    localStorage.setItem('playerTwo', 0)
+    playerOneScore.innerHTML = localStorage.getItem('playerOne')
+    playerTwoScore.innerHTML = localStorage.getItem('playerTwo')
 }
